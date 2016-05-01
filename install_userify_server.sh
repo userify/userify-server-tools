f16f861d5c244265eb28f8ea1247132bf52f280e
Jamieson Becker <jamieson@jamiesonbecker.com>
2016-04-30 19:11:27 -0500

    User interaction for URL

--- install_userify_server.sh
+++ install_userify_server.sh
@@ -3,7 +3,8 @@
 # Userify Server Installer Script
 # Copyright (c) 2016 Userify Corporation
 # By Jamieson Becker
-# curl https://releases.userify.com/install_userify_server.sh | sudo -sE
+# curl https://releases.userify.com/install_userify_server.sh > install_userify_server.sh
+# sudo bash ./install_userify_server.sh
 
 echo "Please paste the URL for your userify server installable."
 read url
