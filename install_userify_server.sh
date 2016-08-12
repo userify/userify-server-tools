c2121ea305beda45d21dffdf90f7323aa8c7a025
Jamieson Becker <jamieson@jamiesonbecker.com>
2016-08-12 11:37:05 -0500

    find /usr/local/bin/pip before sudo

--- install_userify_server.sh
+++ install_userify_server.sh
@@ -82,7 +82,8 @@ sudo which apt-get 2>/dev/null && debian_prereqs
 
 set -e
 PATH="/usr/local/bin/:/usr/local/sbin/:$PATH"
-sudo $(which pip) install --upgrade \
+pip=$(which pip)
+sudo $pip install --upgrade \
     ndg-httpsclient \
     pyasn1 \
     requests \
