230979569f6490473fecdbf0b71239122745c617
Jamieson Becker <jamieson@jamiesonbecker.com>
2016-08-23 20:07:04 -0500

    Added support for older shim versions

--- userify_shim_debug_info.sh
+++ userify_shim_debug_info.sh
@@ -4,22 +4,16 @@ cat << "EOF" |sudo -sE > debug.log 2>&1
 uname -a
 echo
 python --version
-echo
-echo "======================="
-echo /var/log/userify-shim.log 
-cat /var/log/userify-shim.log 
-echo
-echo
-
-for fn in creds.py userify_config.py shim.sh
+    
+for fn in /var/log/userify-shim.log /var/log/shim.log /opt/userify/creds.py /opt/userify/userify_config.py /opt/userify/shim.sh /etc/rc.local
 do
     echo
     echo
     echo "======================="
-    echo /opt/userify/$fn
+    echo $fn
     echo "======================="
     echo
-    grep -hRv api_key /opt/userify/$fn
+    grep -hRv api_key $fn
     echo
     echo
 done
