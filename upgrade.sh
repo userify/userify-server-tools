6b2ce9708c1c9c674734dd8cf904f35090a23da6
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-01-09 14:41:39 -0600

    Added upgrade script.

---
+++ upgrade.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "Usage: $0 EDITION"
+echo "EDITION can be 'enterprise' or 'pro'"
+
+tmpfile=$(mktemp)
+edition="$1"
+if [ -z "$edition" ]; then
+    edition="enterprise"
+fi
+curl "https://releases.userify.com/dist/userify-$edition-server.gz" | gunzip > "$tmpfile"
+sudo mv -v "$tmpfile" /opt/userify-server/userify-server
