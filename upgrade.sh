1c8abc57c6f52395a25cf2d32a7980631327a8cc
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-01-09 16:12:29 -0600

    Better instructions.

--- upgrade.sh
+++ upgrade.sh
@@ -1,15 +1,22 @@
-#! /bin/bash
+#! /bin/bash -e
 
 # example usage:
-# curl https://usrfy.io/upgrade.sh  |sudo bash
-
-echo "Usage: $0 EDITION"
-echo "EDITION can be 'enterprise' or 'pro'"
+# curl https://usrfy.io/upgrade.sh enterprise |sudo bash
 
 tmpfile=$(mktemp)
 edition="$1"
 if [ -z "$edition" ]; then
     edition="enterprise"
+    echo "Usage: $0 EDITION"
+    echo "EDITION defaults to enterprise and can be 'enterprise' or 'pro'. If you are on pro, please press control-C now."
 fi
-curl "https://releases.userify.com/dist/userify-$edition-server.gz" | gunzip > "$tmpfile"
+
+curl -q "https://releases.userify.com/dist/userify-$edition-server.gz" | gunzip > "$tmpfile"
+
+echo "One moment.. you can cancel now by pressing control-C."
+# sleep for a few seconds to allow control-C
+sleep 3
+
 sudo mv -v "$tmpfile" /opt/userify-server/userify-server
+
+echo 'Success! You do not need to do anything else.. Userify will automatically notice the update and restart.'
