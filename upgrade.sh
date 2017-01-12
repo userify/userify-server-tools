183f33f8e88427cc2a476ce08aa65dda09242f9e
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-01-12 13:37:15 -0600

    Added chmod/chown (thx to Brian Daugherty @Fit Pay)

--- upgrade.sh
+++ upgrade.sh
@@ -17,6 +17,9 @@ echo "One moment.. you can cancel now by pressing control-C."
 # sleep for a few seconds to allow control-C
 sleep 3
 
+sudo chmod +x "$tmpfile"
+sudo chown userify-server:userify-server "$tmpfile"
 sudo mv -v "$tmpfile" /opt/userify-server/userify-server
+sudo chown -R userify-server:userify-server /opt/userify-server/
 
 echo 'Success! You do not need to do anything else.. Userify will automatically notice the update and restart.'
