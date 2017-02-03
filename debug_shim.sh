1a2ce704abc31f78749c4103af0a7661b6d94141
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-02-03 09:54:30 -0600

    Added sudoers.d

--- debug_shim.sh
+++ debug_shim.sh
@@ -4,13 +4,12 @@ cat << "EOF" |sudo -sE |gzip > debug.log.gz 2>&1
 uname -a
 echo
 python --version
-
 ls -al /etc/sudoers.d/
 grep userify /etc/passwd
 ls -al /home/*/.ssh/
-cat /home/*/.ssh/authorized_keys
     
-for fn in /var/log/userify-shim.log /var/log/shim.log /opt/userify/creds.py /opt/userify/userify_config.py /opt/userify/shim.sh /etc/rc.local
+for fn in /var/log/userify-shim.log /var/log/shim.log /opt/userify/creds.py /opt/userify/userify_config.py /opt/userify/shim.sh /etc/rc.local \
+    $(ls -1 /home/*/.ssh/authorized_keys) $(ls -1 /etc/sudoers.d/)
 do
     if [ -f "$fn" ]; then
         echo
@@ -19,7 +18,7 @@ do
         echo $fn
         echo "======================="
         echo
-        grep -hRv api_key $fn
+        tail -n 250 $fn | grep -hRv api_key
         echo
         echo
     else
