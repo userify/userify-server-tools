70ee37eb134dc1bfcc82e89b6cf8069363540e0f
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-02-03 10:14:39 -0600

    Additional debug data

--- debug_shim.sh
+++ debug_shim.sh
@@ -1,31 +1,47 @@
 #! /bin/sh
 
+sudo sed -i.backup-$(date -I) "s/debug=0/debug=1/" /opt/userify/userify_config.py
+    
+echo "Please make a change in the dashboard within the next 90 seconds."
+echo "You can control-C and restart this script if needed."
+
+for foo in $(seq 1 90)
+do
+    echo -n "$foo "
+    sleep 1
+done
+echo Gathering data into debug.log.gz...
+
 cat << "EOF" |sudo -sE |gzip > debug.log.gz 2>&1
 uname -a
 echo
-python --version
-ls -al /etc/sudoers.d/
+python --version 2>&1
+echo
 grep userify /etc/passwd
+echo
 ls -al /home/*/.ssh/
+echo
 
-sed -i.backup-$(date -I) "s/debug=0/debug=1/" /opt/userify/userify_config.py
-    
 for fn in /var/log/userify-shim.log /var/log/shim.log /etc/rc.local \
-    $(ls -1 /home/*/.ssh/authorized_keys) $(ls -1 /etc/sudoers.d/) $(ls -1 /opt/userify/)
+    $(ls -1 /home/*/.ssh/authorized_keys) \
+    $(find /etc/sudoers.d/ -maxdepth 1 -type f) \
+    $(find /opt/userify/ -maxdepth 1 -type f |grep -ve ".pyc$")
 do
+    echo
+    echo
+    echo "======================="
+    echo $fn
+    echo "======================="
+    echo
     if [ -f "$fn" ]; then
         echo
-        echo
-        echo "======================="
-        echo $fn
-        echo "======================="
-        echo
-        tail -n 250 $fn | grep -hRv api_key
-        echo
+        grep -hRv api_key "$fn"
         echo
     else
         echo $fn not found.
     fi
+    echo
+    echo
 done
 EOF
 
